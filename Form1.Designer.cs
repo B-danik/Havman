ddbe22cfe24495b532db0985d5c36347a5ffcda8
danik <baybolov_daniyar@mail.ru>
2022-04-20 22:57:39 +0600

    comit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,53 +33,142 @@ namespace Havman
             this.cryptographic_textBox = new System.Windows.Forms.TextBox();
             this.encrypt_button = new System.Windows.Forms.Button();
             this.decrypt_button = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.kod_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // source_textBox
             // 
-            this.source_textBox.Location = new System.Drawing.Point(12, 12);
+            this.source_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.source_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.source_textBox.Location = new System.Drawing.Point(9, 11);
+            this.source_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.source_textBox.Multiline = true;
             this.source_textBox.Name = "source_textBox";
-            this.source_textBox.Size = new System.Drawing.Size(1079, 123);
+            this.source_textBox.Size = new System.Drawing.Size(819, 173);
             this.source_textBox.TabIndex = 0;
             // 
             // cryptographic_textBox
             // 
-            this.cryptographic_textBox.Location = new System.Drawing.Point(12, 154);
+            this.cryptographic_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cryptographic_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cryptographic_textBox.Location = new System.Drawing.Point(9, 201);
+            this.cryptographic_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cryptographic_textBox.Multiline = true;
             this.cryptographic_textBox.Name = "cryptographic_textBox";
-            this.cryptographic_textBox.Size = new System.Drawing.Size(1079, 123);
+            this.cryptographic_textBox.ReadOnly = true;
+            this.cryptographic_textBox.Size = new System.Drawing.Size(819, 173);
             this.cryptographic_textBox.TabIndex = 1;
             // 
             // encrypt_button
             // 
-            this.encrypt_button.Location = new System.Drawing.Point(12, 283);
+            this.encrypt_button.BackColor = System.Drawing.Color.Lime;
+            this.encrypt_button.FlatAppearance.BorderSize = 0;
+            this.encrypt_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypt_button.Location = new System.Drawing.Point(9, 378);
+            this.encrypt_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.encrypt_button.Name = "encrypt_button";
-            this.encrypt_button.Size = new System.Drawing.Size(133, 55);
+            this.encrypt_button.Size = new System.Drawing.Size(123, 45);
             this.encrypt_button.TabIndex = 2;
-            this.encrypt_button.Text = "button1";
-            this.encrypt_button.UseVisualStyleBackColor = true;
+            this.encrypt_button.Text = "Encrypt";
+            this.encrypt_button.UseVisualStyleBackColor = false;
             // 
             // decrypt_button
             // 
-            this.decrypt_button.Location = new System.Drawing.Point(958, 283);
+            this.decrypt_button.BackColor = System.Drawing.Color.Lime;
+            this.decrypt_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decrypt_button.Location = new System.Drawing.Point(136, 378);
+            this.decrypt_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.decrypt_button.Name = "decrypt_button";
-            this.decrypt_button.Size = new System.Drawing.Size(133, 55);
+            this.decrypt_button.Size = new System.Drawing.Size(123, 45);
             this.decrypt_button.TabIndex = 3;
-            this.decrypt_button.Text = "button2";
-            this.decrypt_button.UseVisualStyleBackColor = true;
+            this.decrypt_button.Text = "Decipher";
+            this.decrypt_button.UseVisualStyleBackColor = false;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(9, 429);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.Size = new System.Drawing.Size(383, 217);
+            this.dgv.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(400, 429);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 217);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // clear_button
+            // 
+            this.clear_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clear_button.BackColor = System.Drawing.Color.Red;
+            this.clear_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clear_button.Location = new System.Drawing.Point(727, 378);
+            this.clear_button.Margin = new System.Windows.Forms.Padding(2);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(100, 45);
+            this.clear_button.TabIndex = 10;
+            this.clear_button.Text = "Clear";
+            this.clear_button.UseVisualStyleBackColor = false;
+            // 
+            // kod_button
+            // 
+            this.kod_button.BackColor = System.Drawing.Color.Lime;
+            this.kod_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kod_button.Location = new System.Drawing.Point(263, 378);
+            this.kod_button.Margin = new System.Windows.Forms.Padding(2);
+            this.kod_button.Name = "kod_button";
+            this.kod_button.Size = new System.Drawing.Size(184, 45);
+            this.kod_button.TabIndex = 11;
+            this.kod_button.Text = "Generation Kod";
+            this.kod_button.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1103, 713);
+            this.ClientSize = new System.Drawing.Size(836, 659);
+            this.Controls.Add(this.kod_button);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgv);
             this.Controls.Add(this.decrypt_button);
             this.Controls.Add(this.encrypt_button);
             this.Controls.Add(this.cryptographic_textBox);
             this.Controls.Add(this.source_textBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +180,10 @@ namespace Havman
         private System.Windows.Forms.TextBox cryptographic_textBox;
         private System.Windows.Forms.Button encrypt_button;
         private System.Windows.Forms.Button decrypt_button;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Button kod_button;
     }
 }
 
