9ed450cd0165a681bfd697a63af4dd0ead9ce82c
DronStar <starinenko2251@gmail.com>
2022-04-21 23:31:07 +0600

    havmanFinal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,11 +46,11 @@ namespace Havman
             this.source_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.source_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.source_textBox.Location = new System.Drawing.Point(9, 11);
-            this.source_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.source_textBox.Location = new System.Drawing.Point(12, 14);
+            this.source_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.source_textBox.Multiline = true;
             this.source_textBox.Name = "source_textBox";
-            this.source_textBox.Size = new System.Drawing.Size(819, 173);
+            this.source_textBox.Size = new System.Drawing.Size(1091, 212);
             this.source_textBox.TabIndex = 0;
             // 
             // cryptographic_textBox
@@ -58,12 +58,12 @@ namespace Havman
             this.cryptographic_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cryptographic_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cryptographic_textBox.Location = new System.Drawing.Point(9, 201);
-            this.cryptographic_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cryptographic_textBox.Location = new System.Drawing.Point(12, 247);
+            this.cryptographic_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cryptographic_textBox.Multiline = true;
             this.cryptographic_textBox.Name = "cryptographic_textBox";
             this.cryptographic_textBox.ReadOnly = true;
-            this.cryptographic_textBox.Size = new System.Drawing.Size(819, 173);
+            this.cryptographic_textBox.Size = new System.Drawing.Size(1091, 212);
             this.cryptographic_textBox.TabIndex = 1;
             // 
             // encrypt_button
@@ -71,10 +71,10 @@ namespace Havman
             this.encrypt_button.BackColor = System.Drawing.Color.Lime;
             this.encrypt_button.FlatAppearance.BorderSize = 0;
             this.encrypt_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.encrypt_button.Location = new System.Drawing.Point(9, 378);
-            this.encrypt_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.encrypt_button.Location = new System.Drawing.Point(12, 465);
+            this.encrypt_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.encrypt_button.Name = "encrypt_button";
-            this.encrypt_button.Size = new System.Drawing.Size(123, 45);
+            this.encrypt_button.Size = new System.Drawing.Size(164, 55);
             this.encrypt_button.TabIndex = 2;
             this.encrypt_button.Text = "Encrypt";
             this.encrypt_button.UseVisualStyleBackColor = false;
@@ -84,10 +84,10 @@ namespace Havman
             // 
             this.decrypt_button.BackColor = System.Drawing.Color.Lime;
             this.decrypt_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.decrypt_button.Location = new System.Drawing.Point(136, 378);
-            this.decrypt_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.decrypt_button.Location = new System.Drawing.Point(181, 465);
+            this.decrypt_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.decrypt_button.Name = "decrypt_button";
-            this.decrypt_button.Size = new System.Drawing.Size(123, 45);
+            this.decrypt_button.Size = new System.Drawing.Size(164, 55);
             this.decrypt_button.TabIndex = 3;
             this.decrypt_button.Text = "Decipher";
             this.decrypt_button.UseVisualStyleBackColor = false;
@@ -101,12 +101,12 @@ namespace Havman
             | System.Windows.Forms.AnchorStyles.Left)));
             this.dgv.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(9, 429);
-            this.dgv.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv.Location = new System.Drawing.Point(12, 528);
+            this.dgv.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowHeadersWidth = 51;
-            this.dgv.Size = new System.Drawing.Size(383, 217);
+            this.dgv.Size = new System.Drawing.Size(511, 267);
             this.dgv.TabIndex = 8;
             // 
             // dataGridView1
@@ -119,12 +119,12 @@ namespace Havman
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(400, 429);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(533, 528);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(427, 217);
+            this.dataGridView1.Size = new System.Drawing.Size(569, 267);
             this.dataGridView1.TabIndex = 9;
             // 
             // clear_button
@@ -132,10 +132,10 @@ namespace Havman
             this.clear_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.clear_button.BackColor = System.Drawing.Color.Red;
             this.clear_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.clear_button.Location = new System.Drawing.Point(727, 378);
-            this.clear_button.Margin = new System.Windows.Forms.Padding(2);
+            this.clear_button.Location = new System.Drawing.Point(969, 465);
+            this.clear_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.clear_button.Name = "clear_button";
-            this.clear_button.Size = new System.Drawing.Size(100, 45);
+            this.clear_button.Size = new System.Drawing.Size(133, 55);
             this.clear_button.TabIndex = 10;
             this.clear_button.Text = "Clear";
             this.clear_button.UseVisualStyleBackColor = false;
@@ -144,19 +144,20 @@ namespace Havman
             // 
             this.kod_button.BackColor = System.Drawing.Color.Lime;
             this.kod_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.kod_button.Location = new System.Drawing.Point(263, 378);
-            this.kod_button.Margin = new System.Windows.Forms.Padding(2);
+            this.kod_button.Location = new System.Drawing.Point(351, 465);
+            this.kod_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kod_button.Name = "kod_button";
-            this.kod_button.Size = new System.Drawing.Size(184, 45);
+            this.kod_button.Size = new System.Drawing.Size(245, 55);
             this.kod_button.TabIndex = 11;
-            this.kod_button.Text = "Generation Kod";
+            this.kod_button.Text = "Generation Cod";
             this.kod_button.UseVisualStyleBackColor = false;
+            this.kod_button.Click += new System.EventHandler(this.kod_button_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 659);
+            this.ClientSize = new System.Drawing.Size(1115, 811);
             this.Controls.Add(this.kod_button);
             this.Controls.Add(this.clear_button);
             this.Controls.Add(this.dataGridView1);
@@ -165,7 +166,7 @@ namespace Havman
             this.Controls.Add(this.encrypt_button);
             this.Controls.Add(this.cryptographic_textBox);
             this.Controls.Add(this.source_textBox);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,7 @@ namespace Havman
         {
             protected char symbol;
             protected int count;
+
             public Symbol(char symbol)
             {
                 this.symbol = symbol;
@@ -39,33 +40,148 @@ namespace Havman
                 count++;
             }
         }
+        public class Node
+        {
+            public int num { get; set; }
+            public string str { get; set; }
+            public int hz { get; set; }
+            public int left { get; set; }
+            public int right{ get; set; }
+            public int parent { get; set; }
+            public int action { get; set; }
+
+            public Node(int num, Symbol symbol) 
+            {
+                this.num = num;
+                str = ""+ symbol.getSymbol();
+                hz = symbol.getCount();
+            }
+            public Node(int num,string str,int hz,int left,int right)
+            {
+                this.num = num;
+                this.str = str;
+                this.hz = hz;
+                this.left = left;
+                this.right = right;
+                this.parent = parent;
+            }
+            public static List<Node> convertList(List<Symbol> symbols)
+            {
+                List<Node> nodes = new List<Node>();
+                int i = 1;
+                foreach(Symbol sym in symbols)
+                    nodes.Add(new Node(i++, sym));
+                return nodes;
+            }
+
+
+
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
-        public List<Symbol> Get_symbol()
+        public List<Symbol> getSymbol()
         {
-            List<Symbol> list_sym = new List<Symbol>();
-            string slovo = Convert.ToString(source_textBox.Text);  
-    
-
-            foreach (char Letter in slovo.Distinct().ToArray())
+            List<Symbol> listSym = new List<Symbol>();
+            string text = Convert.ToString(source_textBox.Text);
+            foreach (char letter in text)
             {
-                string str = list_sym.Find(x => x == Letter);
-                list_sym.Add(new Symbol(Letter));
+                Predicate<Symbol> isHasOnList = (Symbol symbol) => symbol.getSymbol() == letter;
+                int index = listSym.FindIndex(isHasOnList);
+
+                if (index >= 0)
+                {
+                    listSym[index].incremCount();
+                }
+                else
+                {
+                    listSym.Add(new Symbol(letter));
+                }
                 
-                //int Count = slovo.Count(chr => chr == Letter);              
-                //if (Count > 1)
-                //{
-                //    Symbol symbol = new Symbol(Letter);
-                //    symbol.incremCount();
-                //   cryptographic_textBox.Text = ($"Слово состоит из различных символов {Letter} в количестве {Count}");
-                //}
-             
             }
 
-            return list_sym;
+            return listSym;
+        }
+        public List<Node> havmen()
+        {
+            List<Node> listNode = Node.convertList(getSymbol());
+            int indexAction = 1;
+            int minHz;
+            while (true)
+            {
+                minHz = source_textBox.Text.Length;
+                int indexFirst=-1;
+                int i = 0;
+                foreach(Node node in listNode)
+                {
+                    
+                    if (node.action == 0)
+                    {
+                        if (node.hz < minHz)
+                        {
+                            minHz = node.hz;
+                            indexFirst = i;
+                        }
+                    }
+                    i++;
+                }
+                if (indexFirst == -1)
+                    break;
+                listNode[indexFirst].action = -1;
+
+                minHz = source_textBox.Text.Length;
+                int indexSecond = -1;
+                i = 0;
+
+                foreach (Node node in listNode)
+                {
+
+                    if (node.action == 0)
+                    {
+                        if (node.hz < minHz)
+                        {
+                            minHz = node.hz;
+                            indexSecond = i;
+                        }
+                    }
+                    i++;
+                }
+                if (indexSecond == -1)
+                    break;
+               
+                string firstSecondStr = listNode[indexFirst].str + listNode[indexSecond].str;
+                int firstSecondHz = listNode[indexFirst].hz + listNode[indexSecond].hz;
+
+                int left, right;
+                if (listNode[indexFirst].hz > listNode[indexSecond].hz)
+                {
+                    right = indexFirst + 1;
+                    left = indexSecond + 1;
+                }
+                else
+                {
+                    right = indexSecond + 1;
+                    left = indexFirst + 1;
+                }
+
+                listNode[indexFirst].parent = listNode.Count+1;
+                listNode[indexSecond].parent = listNode.Count+1;
+
+                listNode[indexFirst].action = indexAction;
+                listNode[indexSecond].action = indexAction;
+                indexAction++;
+
+                listNode.Add(new Node(
+                    listNode.Count + 1,
+                    firstSecondStr,
+                    firstSecondHz,
+                    left,
+                    right
+                    ));
+            }
+            return listNode;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -75,8 +191,12 @@ namespace Havman
 
         private void encrypt_button_Click(object sender, EventArgs e)
         {
-             Get_symbol();
 
+        }
+
+        private void kod_button_Click(object sender, EventArgs e)
+        {
+            havmen();
         }
     }
 }
